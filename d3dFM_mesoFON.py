f13b8c9d82a311054303bb3ea869a565b60def7f
bestbryant <bestbryant@gmail.com>
2021-10-20 14:58:45 +0200

    Trial adding plan for the script

---
+++ d3dFM_mesoFON.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# Script to Coupling Delft3D and MesoFON
+
+# Import the necessary packages
+
+# Initiate the BMI Delft3D
+
+# Read the D3D Domain and Map it to create the 'world' for MesoFON
+
+# Get the limit of the approximated LLWL and build the outer line
+# This is as the base for 'salinity approach' to kill the trees that are 
+# located outside the allowed area
+
+
+
+## Do the first run of Delft3D
+
+# Pause the simulation to a certain delta t coupling (3 months)
+
+## Script to process the output with BMI? or other default Python processing
+## package for DFM
+
+## Retrieve the D3DFM map based on WoO
